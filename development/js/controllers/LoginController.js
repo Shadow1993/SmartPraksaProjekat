60bbaf43353ade4a9813df32aa181fd255acfae3
Gojko Dikić <gdikic@smart.rs>
2017-06-14 10:41:24 +0200

    working on login

--- development/js/controllers/LoginController.js
+++ development/js/controllers/LoginController.js
@@ -3,14 +3,25 @@
 
     var app = angular.module('app');
 
-    app.controller('LoginController', [LoginController]);
+    app.controller('LoginController', ['AuthorizeService', LoginController]);
 
-    function LoginController() {
+    function LoginController(AuthorizeService) {
         var vm = this;
         vm.test = 'test';
 
+        vm.authorize = function() {
+            console.log(vm.user);
+            AuthorizeService.authorize(vm.user)
+                .then(function(res) {
+                    console.log(res);
+                })
+                .catch(function(res) {
+                    throw res;
+                });
+        };
+
         vm.user = {
-            name: '',
+            username: '',
             password: ''
         };
 
