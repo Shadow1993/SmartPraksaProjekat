eb071e88f0f821e31ec350db041230bab3beb51c
Gojko Dikić <gdikic@smart.rs>
2017-06-16 11:05:28 +0200

    Authorization, Login, Roles Done for now, Again..

--- development/js/controllers/HeaderController.js
+++ development/js/controllers/HeaderController.js
@@ -8,7 +8,31 @@
     function HeaderController(AuthorizeService) {
         var vm = this;
         vm.test = 'test';
+        vm.myRole = '';
 
-        console.log(AuthorizeService.isAuthorized());
+        AuthorizeService.checkAuthorization()
+            .then(function () {
+                vm.user = AuthorizeService.getUser();
+                vm.permission = function (role) {
+                    for (var i in vm.user.role) {
+                        if (vm.user.role[i] === role) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+                if (vm.user.role.includes('Viewer')) {
+                    vm.myRole += ' Viewer';
+                }
+                if (vm.user.role.includes('Voter')) {
+                    vm.myRole += ' Voter';
+                }
+                if (vm.user.role.includes('Facilitator')) {
+                    vm.myRole += ' Facilitator';
+                }
+                if (vm.user.role.includes('Administrator')) {
+                    vm.myRole += ' Administrator';
+                }
+            });
     }
 }());
