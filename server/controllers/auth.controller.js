6713cb8dd72ab77d26e75a713e2b4c404ae85ea4
Lazar Đorđević <ldjordjevic@smart.rs>
2017-06-21 15:51:53 +0200

    added db seeding for rolesand admin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -11,79 +11,24 @@ module.exports.isLoggedIn = function (req, res, next) {
 };
 
 module.exports.isAdmin = function (req, res, next) {
-    RoleModel.findOne({ _id: req.user.role })
-        .exec(function (err, roleDb) {
-            if (err) {
-                return next(err.message);
-            } else {
-                console.log(roleDb);
-                if (roleDb.title === 'Administrator') {
-                    return next();
-                } else {
-                    res.status(403).send('You are not authorized');
-                }
-            }
-        });
-};
-
-module.exports.isViewer = function (req, res, next) {
     RoleModel.find({ _id: req.user.role })
         .exec(function (err, roleDb) {
             if (err) {
                 return next(err.message);
             } else {
+                console.log(roleDb);
                 var test = false;
                 for (var i = 0; i < roleDb.length; i++) {
                     console.log(roleDb[i]);
-                    if (roleDb[i].title === 'Viewer') {
+                    if (roleDb[i].title === 'Administrator') {
                         test = true;
                     }
                 }
                 if (test) {
                     next();
                 } else {
-                    res.status(401).send('You are not authorized Viewer');
-                }
-            }
-        })
-};
-
-module.exports.isVoter = function (req, res, next) {
-    RoleModel.find({ _id: req.user.role })
-        .exec(function (err, roleDb) {
-            if (err) {
-                return next(err.message);
-            } else {
-                var test = false;
-                for (var i = 0; i < roleDb.length; i++) {
-                    console.log(roleDb[i]);
-                    if (roleDb[i].title === 'Voter') {
-                        test = true;
-                    }
-                }
-                if (test) {
-                    next();
-                } else {
-                    res.status(401).send('You are not authorized Voter');
-                }
-            }
-        })
-};
-
-module.exports.isFascilitator = function (req, res, next) {
-    RoleModel.findOne({ _id: req.user.role })
-        .exec(function (err, roleDb) {
-            if (err) {
-                return next(err.message);
-            } else {
-                console.log(roleDb);
-                if (roleDb.title === 'Fascilitator') {
-                    return next();
-                } else {
-                    res.status(403).send('You are not authorized Fascilitator');
+                    res.status(401).send('You are not authorized Administrator');
                 }
             }
         });
 };
-
-// populate role i 1 default administrator
--- server/models/role.model.js
+++ server/models/role.model.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+    seedRoleData = require('./seed.role.model'),
     Schema = mongoose.Schema;
 
 var roleSchema = new Schema({
@@ -13,5 +14,35 @@ var roleSchema = new Schema({
         required: 'Title fiels is required!'
     }
 });
+
+var RoleModel = mongoose.model('Role', roleSchema);
+
+// Seeding role model
+RoleModel.find({}, function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        if (data == '') {
+            console.log('ok');
+            for (var i = 0; i < seedRoleData.roles.length; i++) {
+                seed(seedRoleData.roles[i]);
+            }
+        }
+    }
+});
+
+function seed(data) {
+    console.log(data);
+    RoleModel.create({
+        title: data.title,
+        description: data.description
+    }, function (err, roleDb) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(roleDb)
+        }
+    });
+}
 
 module.exports = mongoose.model('Role', roleSchema);
