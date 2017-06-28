054bb24b308222c48049387e913a92443e7a4f7a
Dejan Lončar <dloncar@smart.rs>
2017-06-28 10:26:14 +0200

    komentarisana direktiva za skrolovanje

--- ionic/www/scripts/controllers/FooterController.js
+++ ionic/www/scripts/controllers/FooterController.js
@@ -3,18 +3,23 @@
 
     var app = angular.module('app');
 
-    app.controller('FooterController', ['$ionicScrollDelegate', FooterController]);
+    app.controller('FooterController', ['$ionicScrollDelegate',
+                                        '$rootScope',
+                                        FooterController]);
 
-    function FooterController($ionicScrollDelegate) {
+    function FooterController($ionicScrollDelegate, $rootScope) {
         var vm = this;
 
         vm.scrollTop = function () {
             $ionicScrollDelegate.scrollTop(2000);
         };
+        $rootScope.$on('$stateChangeSuccess', function() {
+            console.log('test');
+        });
     }
 
     // Show or hide scrolling arrow
-    app.directive('scrollWatch', function($rootScope, $ionicScrollDelegate) {
+    /*app.directive('scrollWatch', function($rootScope, $ionicScrollDelegate) {
         return function(scope, elem) {
             var start = 0;
             var threshold = 250;
@@ -30,5 +35,5 @@
                 $rootScope.$apply();
             });
         };
-    });
+    });*/
 }());
