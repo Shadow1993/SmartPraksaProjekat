3e590b44e734774d61bf6131332f02151ef45205
Dušan Ćirić <dciric@smart.rs>
2017-06-28 14:37:21 +0200

    Commented FooterCtrl directive

--- ionic/www/scripts/controllers/FooterController.js
+++ ionic/www/scripts/controllers/FooterController.js
@@ -13,22 +13,21 @@
         };
     }
 
-    // Show or hide scrolling arrow
-    app.directive('scrollWatch', function($rootScope, $ionicScrollDelegate) {
-        return function(scope, elem) {
-            var start = 0;
-            var threshold = 250;
-            elem.bind('scroll', function() {
-                $rootScope.scrollC = $ionicScrollDelegate.getScrollPosition().top;
-                if ($rootScope.scrollC - start > threshold) {
-                    $rootScope.slideHeader = true;
-                    console.log($rootScope.slideHeader);
-                } else {
-                    $rootScope.slideHeader = false;
-                    console.log($rootScope.slideHeader);
-                }
-                $rootScope.$apply();
-            });
-        };
-    });
-}());
+//     // Show or hide scrolling arrow
+//     app.directive('scrollWatch', function($rootScope, $ionicScrollDelegate) {
+//         return function(scope, elem) {
+//             var start = 0;
+//             var threshold = 250;
+//             elem.bind('scroll', function() {
+//                 if ($ionicScrollDelegate.getScrollPosition().top - start > threshold) {
+//                     $rootScope.slideHeader = true;
+//                     console.log($rootScope.slideHeader);
+//                 } else {
+//                     $rootScope.slideHeader = false;
+//                     console.log($rootScope.slideHeader);
+//                 }
+//                 $rootScope.$apply();
+//             });
+//         };
+//     });
+// }());
